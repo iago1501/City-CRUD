5bd33e37bbeec72bd80da8c2b69effb01d7fa18c
Iago Espinoza <iespinoza632@beijaflore.com>
2020-05-08 13:22:27 -0300

    Updates

---
+++ precache-manifest.68b038101cf2a2bde97a6dee62d117d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800df4a104de61e5c376cf6925e9e01d",
+    "url": "/city_crud/index.html"
+  },
+  {
+    "revision": "82a4bcdcfe4da0c15348",
+    "url": "/city_crud/static/css/main.6ff7b80d.chunk.css"
+  },
+  {
+    "revision": "fc9cf6e61faa2d5c3225",
+    "url": "/city_crud/static/js/2.37770b35.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/city_crud/static/js/2.37770b35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a4bcdcfe4da0c15348",
+    "url": "/city_crud/static/js/main.71f18711.chunk.js"
+  },
+  {
+    "revision": "59ad92c924bc5bbe9f27",
+    "url": "/city_crud/static/js/runtime-main.a5e29eec.js"
+  },
+  {
+    "revision": "deafa0be1b781324c72f747730922e67",
+    "url": "/city_crud/static/media/wmap.deafa0be.jpg"
+  }
+]);
